5ede009374939d686b68792d25f18019ad23e332
eshenkulovilias <eshenkulov0828@gmail.com>
2021-08-10 21:00:51 +0600

    Rename create_datetime to created_datetime

--- tasks/models.py
+++ tasks/models.py
@@ -5,4 +5,4 @@ class Task(models.Model):
     body = models.TextField()
     estimated_finish_time = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
-    create_datetime = models.DateTimeField(auto_now_add=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
